f37347161ce19997de45ff2b76e00adabca0e099
KhavekarUtkarsh <urkhavekar@gmail.com>
2026-01-03 15:30:40 +0530

    Types of memory studied

--- 01_Basics/dataTypesSummary.js
+++ 01_Basics/dataTypesSummary.js
@@ -49,3 +49,42 @@
 // console.log(typeof bigNumber); // This will give 'undefined' for bigInt data type.
 
 // console.log(typeof myFunction); // This will give 'function' as the type of myFunction is Function.
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Types of Memory are- Stack anf Heap Memory
+
+// 1) Stack Memory is used in Primitive Data types.
+// 2) Heap Memory is used in Non-Primitive Data types.
+
+
+// 3) In stack we give the copy of one variable's value to the another, so changes made in one variable does't result to cchange the value of another variable.
+
+let myYoutubename = "CodeWithUtkarsh";
+let anotherName = myYoutubename; // Here the value of myYoutubename is copied to anotherName variable.
+anotherName = "Chai Aur Code"; // Here we changed the value of anotherName variable.
+console.log(myYoutubename)
+console.log(anotherName)
+console.log(myYoutubename == anotherName) // This results in False.
+
+// Here as you can see we changed the value of the variable anothername but it does not result in change in the value of variable myYoutubename because in Stack we provide the copy of the data to the variables.
+
+
+//4) In Heap we provide the reference of the value of the one variable to the other variable and not the copy. So changes made in the value of one variable will reasult in making the change the original data.
+
+let userOne={
+    email:"uk@google.com",
+    upi:"uk@upi"
+}
+let userTwo = userOne
+userTwo.email = "UK@google.com"
+console.log(userOne.email)
+console.log(userTwo.email)
+console.log(userOne == userTwo) // This results in True.
+
+// Here we created a userOne object and given the values of email and upi, then we have given the values to the userTwo.
+// So herein Heap memory we provide the reference of the values of one variable to the another.
