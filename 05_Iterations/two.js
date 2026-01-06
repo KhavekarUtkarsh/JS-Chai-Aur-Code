f32ada24967725acefea17300af7b419f9efe250
KhavekarUtkarsh <urkhavekar@gmail.com>
2026-01-06 15:04:10 +0530

    Learned while and do-while loop.

---
+++ 05_Iterations/two.js
@@ -0,0 +1,29 @@
+// while loop
+
+let index = 1
+while (index<= 10) {
+    // console.log(`Value of index is : ${index}`)
+    index ++
+}
+
+
+let myArray = ["flash" , "batman" , "superman"]
+let arr= 0 
+while (arr < myArray.length) {
+    // console.log(`Value is : ${myArray[arr]}`)
+    arr ++
+}
+
+
+let score = 1
+do {
+    // console.log(`Score is : ${score}`)
+    score ++
+} while (score <= 10);
+
+
+let score1 = 11  // Here due to use of do-while loop though the condition of while loop is not satisfied , it will get executed for one time.
+do {
+    // console.log(`Score is : ${score1}`)
+    score1 ++
+} while (score1 <= 10);
